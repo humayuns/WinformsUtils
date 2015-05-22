19c52f4405557b0caccf8bb45a3d6cdf45a8ab2c
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-05-22 12:23:29 +0500

    code for screenshot

--- WinformsUtils/ScreenCapture.Designer.vb
+++ WinformsUtils/ScreenCapture.Designer.vb
@@ -22,8 +22,28 @@ Partial Class ScreenCapture
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(187, 163)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(145, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Take Screenshot"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ScreenCapture
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(539, 355)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "ScreenCapture"
         Me.Text = "ScreenCapture"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 End Class
--- WinformsUtils/ScreenCapture.vb
+++ WinformsUtils/ScreenCapture.vb
@@ -1,3 +1,23 @@
-﻿Public Class ScreenCapture
+﻿
+
+Public Class ScreenCapture
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        CaptureScreen(0, 0, Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, "C:\Users\Humayun\Desktop")
+    End Sub
+
+
+    Private Sub CaptureScreen(x As Double, y As Double, width As Double, height As Double, SavePath As String)
+        Dim ix As Integer, iy As Integer, iw As Integer, ih As Integer
+        ix = Convert.ToInt32(x)
+        iy = Convert.ToInt32(y)
+        iw = Convert.ToInt32(width)
+        ih = Convert.ToInt32(height)
+        Dim image As New Bitmap(iw, ih, System.Drawing.Imaging.PixelFormat.Format32bppArgb)
+        Dim g As Graphics = Graphics.FromImage(image)
+        g.CopyFromScreen(ix, iy, ix, iy, New System.Drawing.Size(iw, ih), CopyPixelOperation.SourceCopy)
+        Dim filename = SavePath & "\" & "screenshot-" & DateTime.Now.ToString().Replace("/", "-").Replace(":", ".") & ".png"
+        image.Save(filename, System.Drawing.Imaging.ImageFormat.Png)
+    End Sub
 
 End Class
