3442353922530821068a3266cf03d8d17b5943bd
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-05-23 23:50:22 +0500

    added code for generate thumbnails form

--- WinformsUtils/GenerateThumbnailsForFolder.Designer.vb
+++ WinformsUtils/GenerateThumbnailsForFolder.Designer.vb
@@ -22,8 +22,90 @@ Partial Class GenerateThumbnailsForFolder
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(393, 321)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(100, 17)
+        Me.CheckBox1.TabIndex = 11
+        Me.CheckBox1.Text = "Generate Large"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(174, 321)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(93, 321)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Folder"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 295)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(481, 20)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.Text = "C:\Users\Humayun\Desktop\HQPhotos"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(481, 277)
+        Me.ListBox1.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 321)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Resize"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GenerateThumbnailsForFolder
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(501, 352)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "GenerateThumbnailsForFolder"
         Me.Text = "GenerateThumbnailsForFolder"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
 End Class
--- WinformsUtils/GenerateThumbnailsForFolder.vb
+++ WinformsUtils/GenerateThumbnailsForFolder.vb
@@ -1,3 +1,93 @@
 ﻿Public Class GenerateThumbnailsForFolder
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        IO.Directory.CreateDirectory(TextBox1.Text & "\thumbnails")
+
+        If CheckBox1.Checked Then IO.Directory.CreateDirectory(TextBox1.Text & "\Large")
+
+        ListBox1.Items.Clear()
+
+        For Each f As String In IO.Directory.GetFiles(TextBox1.Text)
+
+            ListBox1.Items.Add(f)
+
+
+            Dim filename = IO.Path.GetFileName(f)
+            If Not filename = "Thumbs.db" Then
+
+                Try
+                    Dim bmp = New Bitmap(TextBox1.Text & "\" & filename)
+                    Dim img = ResizeImage(bmp, New Size(250, 20000), True)
+
+                    Dim newLargeHeight = bmp.Height
+                    If bmp.Height > 1000 Then
+                        newLargeHeight = 1000
+                    End If
+
+                    If CheckBox1.Checked Then
+                        Dim img2 = ResizeImage(bmp, New Size(20000, newLargeHeight), True)
+                        img2.Save(TextBox1.Text & "\large\" & filename)
+                        img2.Dispose()
+                    End If
+
+                    img.Save(TextBox1.Text & "\thumbnails\" & filename)
+                    bmp.Dispose()
+                    img.Dispose()
+
+
+                    GC.Collect()
+                Catch ex As OutOfMemoryException
+                    GC.Collect()
+
+                End Try
+
+
+            End If
+
+        Next
+
+        MsgBox("Reize is done!", vbInformation)
+    End Sub
+
+
+
+    Private Function ResizeImage(ByVal image As Image, _
+ByVal size As Size, Optional ByVal preserveAspectRatio As Boolean = True) As Image
+        Dim newWidth As Integer
+        Dim newHeight As Integer
+        If preserveAspectRatio Then
+            Dim originalWidth As Integer = image.Width
+            Dim originalHeight As Integer = image.Height
+            Dim percentWidth As Single = CSng(size.Width) / CSng(originalWidth)
+            Dim percentHeight As Single = CSng(size.Height) / CSng(originalHeight)
+            Dim percent As Single = If(percentHeight < percentWidth,
+                    percentHeight, percentWidth)
+            newWidth = CInt(originalWidth * percent)
+            newHeight = CInt(originalHeight * percent)
+        Else
+            newWidth = size.Width
+            newHeight = size.Height
+        End If
+        Dim newImage As Image = New Bitmap(newWidth, newHeight)
+        Using graphicsHandle As Graphics = Graphics.FromImage(newImage)
+            graphicsHandle.InterpolationMode = Drawing2D.InterpolationMode.HighQualityBicubic
+            graphicsHandle.DrawImage(image, 0, 0, newWidth, newHeight)
+        End Using
+        Return newImage
+    End Function
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox1.Text <> "" Then
+            FolderBrowserDialog1.SelectedPath = TextBox1.Text
+        End If
+        If FolderBrowserDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Text = ""
+        ListBox1.Items.Clear()
+    End Sub
 End Class
