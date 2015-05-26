66a194e50577915d3c264b052bd71ae324ecfe96
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-05-26 11:18:43 +0500

    Added JSON Editor

---
+++ WinformsUtils/JSONEditor.vb
@@ -0,0 +1,7 @@
+﻿Public Class JSONEditor
+
+    Private Sub JSONEditor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Build a JSON Editor like this
+        ' http://www.codeproject.com/Articles/282865/A-simple-JSON-Editor
+    End Sub
+End Class
