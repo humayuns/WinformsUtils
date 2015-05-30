d623c85b1c0684cd328d7e3644335471ea16e8af
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-05-30 17:48:28 +0500

    load image from web

--- WinformsUtils/WebContent.vb
+++ WinformsUtils/WebContent.vb
@@ -1,3 +1,13 @@
-﻿Public Class WebContent
+﻿Imports System.Net
+Imports System.IO
 
+Public Class WebContent
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim tClient As WebClient = New WebClient
+
+        Dim tImage As Bitmap = Bitmap.FromStream(New MemoryStream(tClient.DownloadData(TextBox1.Text)))
+
+        PictureBox1.Image = tImage
+    End Sub
 End Class
