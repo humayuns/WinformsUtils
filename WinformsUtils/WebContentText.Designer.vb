b28fd4c5332e762c7a3682e6fee8288f4ea53d3e
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-06-01 20:55:48 +0500

    added ui

--- WinformsUtils/WebContentText.Designer.vb
+++ WinformsUtils/WebContentText.Designer.vb
@@ -22,16 +22,50 @@ Partial Class WebContentText
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.btnLoad = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(705, 336)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 354)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(624, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(642, 351)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(75, 23)
+        Me.btnLoad.TabIndex = 2
+        Me.btnLoad.Text = "Load"
+        Me.btnLoad.UseVisualStyleBackColor = True
         '
         'WebContentText
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(729, 451)
+        Me.ClientSize = New System.Drawing.Size(729, 387)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
         Me.Name = "WebContentText"
         Me.Text = "WebContentText"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents btnLoad As System.Windows.Forms.Button
 End Class
