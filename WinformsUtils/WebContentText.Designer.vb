5377606e55fee0baeb791857165cb7b44fa061d1
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-06-02 02:11:48 +0500

    download web content code

--- WinformsUtils/WebContentText.Designer.vb
+++ WinformsUtils/WebContentText.Designer.vb
@@ -22,25 +22,26 @@ Partial Class WebContentText
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txtContent = New System.Windows.Forms.TextBox()
+        Me.txtUrl = New System.Windows.Forms.TextBox()
         Me.btnLoad = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'txtContent
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(705, 336)
-        Me.TextBox1.TabIndex = 0
+        Me.txtContent.Location = New System.Drawing.Point(12, 12)
+        Me.txtContent.Multiline = True
+        Me.txtContent.Name = "txtContent"
+        Me.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtContent.Size = New System.Drawing.Size(705, 336)
+        Me.txtContent.TabIndex = 0
         '
-        'TextBox2
+        'txtUrl
         '
-        Me.TextBox2.Location = New System.Drawing.Point(12, 354)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(624, 20)
-        Me.TextBox2.TabIndex = 1
+        Me.txtUrl.Location = New System.Drawing.Point(12, 354)
+        Me.txtUrl.Name = "txtUrl"
+        Me.txtUrl.Size = New System.Drawing.Size(624, 20)
+        Me.txtUrl.TabIndex = 1
         '
         'btnLoad
         '
@@ -57,15 +58,15 @@ Partial Class WebContentText
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(729, 387)
         Me.Controls.Add(Me.btnLoad)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtUrl)
+        Me.Controls.Add(Me.txtContent)
         Me.Name = "WebContentText"
         Me.Text = "WebContentText"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtContent As System.Windows.Forms.TextBox
+    Friend WithEvents txtUrl As System.Windows.Forms.TextBox
     Friend WithEvents btnLoad As System.Windows.Forms.Button
 End Class
--- WinformsUtils/WebContentText.vb
+++ WinformsUtils/WebContentText.vb
@@ -1,3 +1,10 @@
-﻿Public Class WebContentText
+﻿Imports System.Net
 
+Public Class WebContentText
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        Using client = New WebClient()
+            txtContent.Text = client.DownloadString(txtUrl.Text)
+        End Using
+    End Sub
 End Class
