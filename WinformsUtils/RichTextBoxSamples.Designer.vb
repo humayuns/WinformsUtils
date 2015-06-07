a3ee3872101ce863a1c87e5f696ce026e9eca08e
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-06-07 22:03:40 +0500

    set bold code

--- WinformsUtils/RichTextBoxSamples.Designer.vb
+++ WinformsUtils/RichTextBoxSamples.Designer.vb
@@ -22,8 +22,12 @@ Partial Class RichTextBoxSamples
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(RichTextBoxSamples))
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'RichTextBox1
@@ -43,18 +47,42 @@ Partial Class RichTextBoxSamples
         Me.ListBox1.Size = New System.Drawing.Size(120, 264)
         Me.ListBox1.TabIndex = 1
         '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(650, 25)
+        Me.ToolStrip1.TabIndex = 2
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(35, 22)
+        Me.ToolStripButton1.Text = "Bold"
+        '
         'RichTextBoxSamples
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(650, 350)
+        Me.Controls.Add(Me.ToolStrip1)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.RichTextBox1)
         Me.Name = "RichTextBoxSamples"
         Me.Text = "RichTextBoxSamples"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
     Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents ToolStripButton1 As System.Windows.Forms.ToolStripButton
 End Class
--- WinformsUtils/RichTextBoxSamples.vb
+++ WinformsUtils/RichTextBoxSamples.vb
@@ -1,3 +1,9 @@
 ﻿Public Class RichTextBoxSamples
 
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        RichTextBox1.SelectionFont = New Font(RichTextBox1.Font, FontStyle.Bold)
+        RichTextBox1.SelectionStart = RichTextBox1.SelectionStart + RichTextBox1.SelectionLength
+        RichTextBox1.SelectionLength = 0
+        RichTextBox1.SelectionFont = RichTextBox1.Font
+    End Sub
 End Class
