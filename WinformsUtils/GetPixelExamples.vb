f0a6d003308dd92bc2af367ebbf8177fd87e1023
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-06-10 02:20:40 +0500

    added exception handling

--- WinformsUtils/GetPixelExamples.vb
+++ WinformsUtils/GetPixelExamples.vb
@@ -12,10 +12,14 @@
 
     Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
         If e.Button = Windows.Forms.MouseButtons.Left Then
-            Using MyBMP As New Bitmap(PictureBox1.Image)
-                Dim MyColor As Color = MyBMP.GetPixel(e.X, e.Y)
-                PictureBox2.BackColor = MyColor
-            End Using
+            Try
+                Using MyBMP As New Bitmap(PictureBox1.Image)
+                    Dim MyColor As Color = MyBMP.GetPixel(e.X, e.Y)
+                    PictureBox2.BackColor = MyColor
+                End Using
+            Catch ex As Exception
+
+            End Try
         End If
     End Sub
 End Class
