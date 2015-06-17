fbd93e4240350dcc81b6f26b2accc6e41ad11429
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-06-17 19:40:17 +0500

    added create file and directory rad functions

--- WinformsUtils/RADFunctions.vb
+++ WinformsUtils/RADFunctions.vb
@@ -3,4 +3,16 @@
         Dim tClient As Net.WebClient = New Net.WebClient
         Return Bitmap.FromStream(New IO.MemoryStream(tClient.DownloadData(url)))
     End Function
+
+    Public Shared Sub CreateFolderIfNotExists(path As String)
+        If Not IO.Directory.Exists(path) Then
+            IO.Directory.CreateDirectory(path)
+        End If
+    End Sub
+
+    Public Shared Sub CreateFileIfNotExists(filename As String)
+        If Not IO.File.Exists(filename) Then
+            IO.File.Create(filename).Dispose()
+        End If
+    End Sub
 End Class
