3928b395dfe2d4f5ea456b77ef1af5832c69d792
Humayun Shabbir <humayun_shabbir@hotmail.com>
2016-04-05 16:51:05 +0500

    Added tip to AutocorrectForm.

--- WinformsUtils/AutocorrectForm.Designer.vb
+++ WinformsUtils/AutocorrectForm.Designer.vb
@@ -23,6 +23,7 @@ Partial Class AutocorrectForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -32,11 +33,22 @@ Partial Class AutocorrectForm
         Me.TextBox1.Size = New System.Drawing.Size(471, 20)
         Me.TextBox1.TabIndex = 0
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 79)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(469, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Tip: Type a single i and it will automatically be capitalized as I. And havent is" &
+    " converted to haven't."
+        '
         'AutocorrectForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(506, 120)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Name = "AutocorrectForm"
         Me.Text = "AutocorrectForm"
@@ -45,4 +57,5 @@ Partial Class AutocorrectForm
 
     End Sub
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As Label
 End Class
