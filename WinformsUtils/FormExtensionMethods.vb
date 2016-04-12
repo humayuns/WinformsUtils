6581968460bd4fa55354a32bc88d552c43cc42a1
Humayun Shabbir <humayun_shabbir@hotmail.com>
2016-04-13 00:15:40 +0500

    Added FormExtensionMethods module.

---
+++ WinformsUtils/FormExtensionMethods.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module FormExtensionMethods
+    <Extension()>
+    Sub DrawEllipse(form As Form, pen As Pen, x As Integer, y As Integer, width As Integer, height As Integer)
+        Using g = form.CreateGraphics()
+            'g.DrawLine(New Pen(Brushes.Black), e.X, e.Y, e.X, e.Y)
+            g.DrawEllipse(pen, x, y, height, width)
+        End Using
+    End Sub
+End Module
