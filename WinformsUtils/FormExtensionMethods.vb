469cbced4ab258bdea9590ae601f54f12e73b7f7
Humayun Shabbir <humayun_shabbir@hotmail.com>
2016-05-03 18:39:16 +0500

    Added PSet Form Extension Method. :zap:

--- WinformsUtils/FormExtensionMethods.vb
+++ WinformsUtils/FormExtensionMethods.vb
@@ -8,4 +8,10 @@ Module FormExtensionMethods
             g.DrawEllipse(pen, x, y, height, width)
         End Using
     End Sub
+
+    Sub PSet(form As Form, pen As Pen, x As Integer, y As Integer)
+        Using g = form.CreateGraphics()
+            g.DrawRectangle(pen, x, y, 1, 1)
+        End Using
+    End Sub
 End Module
